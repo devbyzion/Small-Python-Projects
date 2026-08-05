40a853fbec2e81551af1a35d1653d0198c6b0c02
Zion <zion10cummings@gmail.com>
2026-08-05 14:26:27 +0200

    Different versions of my mini RPG game where i demonstrated encapsulation, constructors, methods, inheritance, method overiding and super1

---
+++ RPG/fresh_oop.py
@@ -0,0 +1,29 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.mana = 100
+        
+    def greet(self):
+        print(f"Welcome {self.name}!")
+    
+    def stats(self):
+            print("\n --- Player Stats ---")
+            print(f"Name: {self.name}")
+            print(f"Health: {self.health}")
+            print(f"Mana: {self.mana}")
+            
+class Warrior(Player):
+    def slash(self):
+        print(f"{self.name} used Slash")
+        
+    def greet(self):
+        super().greet()
+        print(f"{self.name} the Warrior eneters the battlefield")
+        
+
+warrior = Warrior("Zion")
+
+warrior.greet()
+warrior.slash()
+warrior.stats()
--- RPG/oop_rpg.py
+++ RPG/oop_rpg.py
@@ -1,15 +1,28 @@
 # Menu
 menu = ("1. Attack. \n2. Heal. \n3. View Stats. \n4. Exit.")
+class_type = ("1. Warrior. \n2. Mage. \n3. Archer")
 
 # Greeting
-print("Welcome curious adventurer!\n",
-       "Please start by setting your details.\n")
+print("Welcome curious adventurer! \nPlease start by setting your details.\n")
+
+print(class_type)
+
+t_class = input("Which class of hero would you like to be? \n")
+
+if t_class not in class_type:
+    print("Invalid Option")
+    exit()
     
+    # User inputs for Class of PLayer
+name = input("The name of the Adventurer?:\n")
+enemy = input("Please state the name of the enemy:\n")
+
+print(f"Welcome {name} the {t_class}! \n")
+
 # Class of players
 class Player:
-    def __init__(self, name, age):
+    def __init__(self, name):
         self.name = name
-        self.age = age
         self.health = 100
         self.mana = 90    
         
@@ -18,10 +31,6 @@ class Player:
         if self.health <= 0:
             self.health = 0
             print("You have dieddd!!!!")
-            
-    def attack(self, enemy, damage):
-        enemy.take_dmg(damage)
-        print(f"{self.name} attacked {enemy.name} for {damage} health")
     
     def heal(self, heals):
         self.health += heals
@@ -44,36 +53,57 @@ class Player:
             print("You are at maxed mana")
         
     def greet(self):
-        print(f"Welcome, {self.name}")
+        print(f"Welcome, {self.name} the {t_class}")
     
-    def stats(self,):
+    def stats(self):
         print("\n --- Player Stats ---")
         print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
         print(f"Health: {self.health}")
         print(f"Mana: {self.mana}")
+
+
+class Warrior(Player):
+    def slash(self, enemy):
+        enemy.take_dmg(35)
+        print(f"{name} slashed {enemy} for 35 damage!")
         
-
-    # User inputs for Class of PLayer
-name = input("The name of the Adventurer?:\n")
-enemy = input("Please state the name of the enemy:\n")
-
-     
-try:
-    age = int(input(f"How old is {name}?\n"))
-except ValueError:
-    print("Please enter a number.")
     
-try:
-    e_age = int(input(f"How old is {enemy}? \n"))
-except ValueError:
-    print("Please enter a number.")
+    def bash(self, enemy):
+        enemy.take_dmg(20)
+        print(f"{name} bashed {enemy} for 20 damage!")
+    
+    def shove(self, enemy):
+        enemy.take_dmg(10)
+        print(f"{name} shoved {enemy} for 10 damage")
+
+class Mage(Player):
+    def fire_ball(self, enemy):
+        enemy.take_dmg(50)
+        
+    def staff_strike(self, enemy):
+        enemy.take_dmg(25)
+    
+    def lightning(self, enemy):
+        enemy.take_dmg(30)
+
+class Archer(Player):
+    def arrow_shot(self, enemy):
+        enemy.take_dmg(25)
+    
+    def poison_arrow(self, enemy):
+        enemy.take_dmg(40)
+    
+    def multi_shot(self, enemy):
+        enemy.take_dmg(35)
+    
+# Class info
+
     
 # Player info
-player2 = Player(enemy, e_age)
-player1 = Player(name, age)
+warrior1 = Warrior(name)
+player2 = Player(enemy)
     
-while player1.health > 0 and player2.health > 0:
+while warrior1.health > 0 and player2.health > 0:
     print(menu)
     try:
         option = int(input(f"What would you like {name} to do?:\n"))
@@ -86,7 +116,7 @@ while player1.health > 0 and player2.health > 0:
         continue
     
     elif option == 1:
-        player1.attack(player2, 20)
+        warrior1.slash(player2)
         if player2.health <= 0:
             print(f"Game over {enemy} has died!")
             break
@@ -94,11 +124,11 @@ while player1.health > 0 and player2.health > 0:
             continue
     
     elif option == 2:
-        player1.heal(20)
+        warrior1.heal(20)
         continue
     
     elif option == 3:
-        player1.stats()
+        warrior1.stats()
         player2.stats()
     
     elif option == 4:
