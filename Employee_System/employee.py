7fd17a96737fa2394ca414715fe6d927eb2949fa
Zion <zion10cummings@gmail.com>
2026-09-03 20:14:07 +0200

    Added more variables

--- Employee_System/employee.py
+++ Employee_System/employee.py
@@ -1,7 +1,9 @@
 class Employee:
     
-    def __init__(self, name, salary):
+    def __init__(self, id, name, position, salary):
+        self.id = id
         self.name = name
+        self.position = position
         self.salary = salary
         
     def display_info(self):
